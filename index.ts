c0aebcf90b45b72efac2604490f6b90a72884dca
MijailStell <mijailstell@gmail.com>
2021-01-30 16:44:51 -0500

    add cors

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
+import bodyParser from "body-parser";
+import cors from 'cors';
 import Server from "./classes/server";
 import router from "./routes/router";
 
 const server = new Server();
+
+// BodyParser
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+
+// CORS
+server.app.use(cors({ origin: true, credentials: true}));
 
 server.app.use('/', router)
 
